653c6a5311f3635d8c8656dec8248b12979efc23
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-06-08 20:56:06 +0200

    mejoras

--- cliente/api-client/api-connector.php
+++ cliente/api-client/api-connector.php
@@ -3,7 +3,7 @@
 
 function ApiRequest(array $data, string $uri, string $query, string $method) // las credenciales se deben incorporar en data. 
 {
-    require "/root/proyecto/vendor/autoload.php";
+    require '../../vendor/autoload.php';
     $client = new GuzzleHttp\Client(
         ['base_uri' => $uri,
             'headers' => [
@@ -24,4 +24,3 @@ $data[] = ([
 
 $result=ApiRequest($data,"http://localhost:5000","login","POST");
 print_r($result);
-?
