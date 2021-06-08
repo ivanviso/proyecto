ac554aeb28e9e1e8166b2047fa85c89ce0f758b8
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-06-08 23:02:19 +0200

    cambios

--- api/api.py
+++ api/api.py
@@ -10,11 +10,15 @@ import wgtools
 import sqlite3
 
 con = sqlite3.connect('wgapi.db')
-with open(r'api/config.yaml') as file:
-    config = yaml.load(file, Loader=yaml.FullLoader)
-config_ldap=config['ldap']
-config_vpn=config['vpn']
-print(config_vpn)
+
+def loadconfig():
+    with open(r'api/config.yaml') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+    global config_ldap,config_vpn
+    config_ldap=config['ldap']
+    config_vpn=config['vpn']
+
+
 
 def ldaplogin(host,user,dn,psw):
     from ldap3 import Server, Connection, ALL
@@ -40,20 +44,23 @@ def home():
 @app.route('/login',methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        loadconfig()
+        new_ip=None
         reserved_ips=list()
-        reserved_ips.extend(ipaddress.ip_network(config_vpn['node-ip']).hosts())
         for ip in config_vpn['reserved-ips'] :
             reserved_ips.extend(ipaddress.ip_network(ip).hosts())
-        avaliable_ip=set(ipaddress.ip_network('10.0.0.0/24').hosts())-set(reserved_ips)
+        avaliable_ip=set(ipaddress.ip_network('10.0.0.0/8').hosts())-set(reserved_ips)
         new_ip=str(next(iter(avaliable_ip)))
         public_key, private_key = wgtools.keypair()
         psk = wgtools.genpsk()
+        reserved_ips.append(ipaddress.ip_address(new_ip))
+        print(reserved_ips)
         values = {
         "public_key": public_key,
         "private_key": private_key,
         "psk": psk,
         "ip": new_ip,
-        "route_networks": config_vpn['routing-subnets']
+        "route_networks": config_vpn['routing-subnets'],
         }
     return jsonify(values)        
 
